33249ed610200196fe30cba8f56c0fdd230f84f8
andreybyrsa <byrsa2004@bk.ru>
2023-07-26 14:45:31 +0500

    Сделал страницу ProfilePage

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,41 @@
+import { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+import { useOutletContext } from 'react-router-dom'
+
+import CreateUserForm from '@Components/CreateUserForm'
+import Button from '@Components/Button'
+
+import PageLayout from '@Layouts/PageLayout'
+
+import useAuth from '@Hooks/useAuth'
+
+import { removeUser } from '@Store/reducers/user/UserReducer'
+
+import './ProfilePage.scss'
+
+function ProfilePage() {
+  const header = useOutletContext()
+  const dispatch = useDispatch()
+
+  const [currentUser] = useAuth()
+
+  const handleLogout = useCallback(() => {
+    dispatch(removeUser())
+
+    localStorage.removeItem('user')
+  }, [dispatch])
+
+  return (
+    <PageLayout className="profile-page">
+      <div className="profile-page__content">
+        <CreateUserForm isLogin={false} />
+        <pre>{JSON.stringify(currentUser, null, 4)}</pre>
+
+        <Button onClick={handleLogout}>Выйти из акканута</Button>
+        {header}
+      </div>
+    </PageLayout>
+  )
+}
+
+export default ProfilePage
