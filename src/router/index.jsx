b841d1c7b643933e249b7b9453641e0379da1db3
andreybyrsa <byrsa2004@bk.ru>
2023-07-26 14:46:42 +0500

    Настроил роутер страниц в проекте

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,11 +1,34 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import IndexPage from '@Pages/IndexPage'
+import ProfilePage from '@Pages/ProfilePage'
+import AuthPage from '@Pages/AuthPage'
+import ErrorPage from '@Pages/ErrorPage'
+
+import AuthMiddleware from '@Middlewares/AuthMiddleware'
+
+import loadIdeas from '@Utils/loadIdeas'
 
 const router = createBrowserRouter([
   {
-    path: '',
-    element: <IndexPage />,
+    element: <AuthMiddleware />,
+    children: [
+      {
+        path: '/',
+        element: <IndexPage />,
+        errorElement: <ErrorPage />,
+        loader: loadIdeas,
+      },
+      {
+        path: '/profile',
+        element: <ProfilePage />,
+        errorElement: <ErrorPage />,
+      },
+    ],
+  },
+  {
+    path: '/auth',
+    element: <AuthPage />,
   },
 ])
 
