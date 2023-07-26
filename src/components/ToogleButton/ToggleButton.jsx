edef0a04e2973f03ac08f690cd8fc6e5bfc2a277
andreybyrsa <byrsa2004@bk.ru>
2023-07-26 14:35:33 +0500

    Сделал компонент ToggleButton

---
+++ src/components/ToogleButton/ToggleButton.jsx
@@ -0,0 +1,37 @@
+import classNames from 'classnames'
+import { useCallback, useRef } from 'react'
+
+import './ToggleButton.scss'
+
+function ToggleButton({ className, value, setValue }) {
+  const switcherRef = useRef(null)
+  const isActive = switcherRef.current?.classList.contains(
+    'toggle-button__switcher--active',
+  )
+
+  const ToggleButtonClassName = classNames('toggle-button', className)
+
+  const SwitcherCalssName = classNames('toggle-button__switcher', {
+    'toggle-button__switcher--active': value,
+    'toggle-button__switcher--disabled': isActive && !value,
+  })
+
+  const handleToggle = useCallback(() => {
+    setValue((prevState) => !prevState)
+  }, [setValue])
+
+  return (
+    <button
+      type="button"
+      className={ToggleButtonClassName}
+      onClick={handleToggle}
+    >
+      <div
+        className={SwitcherCalssName}
+        ref={switcherRef}
+      />
+    </button>
+  )
+}
+
+export default ToggleButton
