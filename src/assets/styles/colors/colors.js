9163dc90e0fa1222da83bdd0b0e9f9f4c6032c9e
andreybyrsa <byrsa2004@bk.ru>
2023-07-26 14:43:15 +0500

    Обновил настройки стилей проекта

--- src/assets/styles/colors/colors.js
+++ src/assets/styles/colors/colors.js
@@ -1,6 +1,11 @@
 const Colors = {
   'primary-color': '#2151FF',
+  'lighted-color': '#c0c0c0',
   'white-color': '#FFFFFF',
+  'success-color': '#25c519',
+  'danger-color': '#FF0404',
+
+  'default-backgroud-color': '#efefef',
 }
 
 export default Colors
