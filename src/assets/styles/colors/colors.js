ad303783382bbaa059906f6b3c9189db49d5c672
andreybyrsa <byrsa2004@bk.ru>
2023-07-27 13:49:33 +0500

    Добавил новые цвета

--- src/assets/styles/colors/colors.js
+++ src/assets/styles/colors/colors.js
@@ -1,8 +1,11 @@
 const Colors = {
   'primary-color': '#2151FF',
-  'lighted-color': '#c0c0c0',
+  'lighted-color': '#9A9A9A',
+  'black-color': '#000000',
   'white-color': '#FFFFFF',
   'success-color': '#25c519',
+  'warning-color': '#F4DC04',
+  'medium-color': '#DBAB0c',
   'danger-color': '#FF0404',
 
   'default-background-color': '#efefef',
