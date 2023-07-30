5c96eb0b88fcf9a13d3bdf210c2613810b7b8a42
kivaX9 <kirill.vlasov.05@inbox.tu>
2023-07-30 23:26:34 +0500

    Сверстал страницу настройки

--- src/pages/SettingPage/SettingPage.jsx
+++ src/pages/SettingPage/SettingPage.jsx
@@ -4,36 +4,109 @@ import Typography from '@Components/Typography'
 import Button from '@Components/Button'
 import Input from '@Components/Input'
 
+import './SettingPage.scss'
+
 import { useState } from 'react'
 import SelectRole from '@Components/SelectRole'
 
 function SettingPage() {
   const [searchValue, setSearchValue] = useState('')
+  const [searchValueFile, setSearchValueFile] = useState('')
+  const [emailArray, setEmailArray] = useState(false)
+  const [emailFile, setEmailFile] = useState(true)
+  const [inputs, setInputs] = useState([])
+  const [value, setValue] = useState('')
+
+  const handleFile = () => {
+    setEmailArray(false)
+    setEmailFile(true)
+  }
+
+  const handleArray = () => {
+    setEmailArray(true)
+    setEmailFile(false)
+  }
+
+  let activeClass = ''
+  let disabledClass = ''
+
+  if (emailArray) {
+    activeClass = 'fs-4 text-primary'
+  } else {
+    activeClass = 'fs-4'
+  }
+
+  if (emailFile) {
+    disabledClass = 'fs-4 text-primary'
+  } else {
+    disabledClass = 'fs-4'
+  }
+
+  const AddInput = () => {
+    setInputs((prevState) => [
+      ...prevState,
+      { value, setValue, id: inputs.length },
+    ])
+  }
 
   return (
     <PageLayout
-      contentClassName="setting-page__content"
+      contentClassName="setting-page h-100"
       leftSidebar={<LeftSideBar />}
     >
-      <div>
-        <div>
-          <Typography className="fs-4 text-primary">Вручную</Typography>
-          <Typography className="fs-4 text-primary">Файлом</Typography>
+      <div className="setting-page__card card card-body">
+        <div className="setting-page__card-nav">
+          <Button onClick={handleArray}>
+            <Typography className={activeClass}>Вручную</Typography>
+          </Button>
+          <Button onClick={handleFile}>
+            <Typography className={disabledClass}>Файлом</Typography>
+          </Button>
         </div>
-        <div>
-          <Input
-            value={searchValue}
-            setValue={setSearchValue}
-            placeholder="Почта"
-          />
-          <Input
-            value={searchValue}
-            setValue={setSearchValue}
-            placeholder="Роль"
-          />
-        </div>
-        <Button className="btn-primary w-100">Отправить</Button>
-        <SelectRole/>
+
+        {emailArray && (
+          <div className="setting-page__card-data w-100">
+            <div className="setting-page__card-emails w-100">
+              <Input
+                value={searchValue}
+                setValue={setSearchValue}
+                placeholder="Почта"
+              />
+              <Button
+                onClick={AddInput}
+                className="btn-primary"
+                iconName="bi bi-plus-lg text-white"
+              />
+            </div>
+            {inputs.map((e) => (
+              <Input
+                key={e.id}
+                value={e.value}
+                setValue={e.setValue}
+                placeholder="Почта"
+              />
+            ))}
+            <SelectRole />
+          </div>
+        )}
+
+        {emailFile && (
+          <div className="setting-page__card-data w-100">
+            <div className="setting-page__card-emails w-100">
+              <Input
+                type="file"
+                value={searchValueFile}
+                setValue={setSearchValueFile}
+                placeholder="Почта"
+              />
+            </div>
+            <SelectRole />
+          </div>
+        )}
+
+        <Button className="setting-page__card-button btn-primary w-100">
+          Отправить
+        </Button>
       </div>
     </PageLayout>
   )
